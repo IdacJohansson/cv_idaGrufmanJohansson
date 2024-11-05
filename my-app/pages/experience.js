154ac9ac01ac7c5a145bcc6ec8ce145f3db644b2
Ida Johansson <ida.johansson90@hotmail.com>
2024-11-05 14:23:47 +0100

    media-q for contact

--- my-app/pages/experience.js
+++ my-app/pages/experience.js
@@ -4,6 +4,8 @@ import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "animate.css";
+import variables from "../styles/variables.module.scss";
+import ProgressBar from "react-bootstrap/ProgressBar";
 import Header from "@/components/Header";
 
 import { fetchExperience } from "../utils/api";
@@ -29,6 +31,12 @@ export async function getServerSideProps() {
 }
 
 const ExperiencePage = ({ experience }) => {
+  const levelProgressMap = {
+    Advanced: 100,
+    Intermediate: 50,
+    Beginner: 30,
+  };
+
   return (
     <main className="d-flex flex-column justify-content-center align-items-center mt-5">
       <Header title="Experience" />
@@ -36,7 +44,7 @@ const ExperiencePage = ({ experience }) => {
       <Card className="container-fluid container-size card-style d-flex flex-column justify-content-center align-items-center mt-5 mb-5">
         <Card.Body className="col-12 col-md-10">
           {/* Work experience */}
-          <Card.Title className="titel-experience d-flex justify-content-center align-items-center">
+          <Card.Title className="titel-experience d-flex justify-content-center align-items-center mb-5">
             Work experience
           </Card.Title>
           <Row className="justify-content-center align-items-center mb-3">
@@ -69,7 +77,7 @@ const ExperiencePage = ({ experience }) => {
         <div className="title-underline"></div>
         {/* Development experience */}
         <Card.Body className="col-12 col-md-10">
-          <Card.Title className="titel-experience d-flex justify-content-center align-items-center">
+          <Card.Title className="titel-experience d-flex justify-content-center align-items-center mb-5">
             Development experience
           </Card.Title>
           <Row className="justify-content-center align-items-center ">
@@ -105,40 +113,36 @@ const ExperiencePage = ({ experience }) => {
         <div className="title-underline"></div>
         {/* Technical Skills */}
         <Card.Body className="col-12 col-md-10">
-          <Card.Title className="titel-experience d-flex justify-content-center align-items-center">
+          <Card.Title className="titel-experience d-flex justify-content-center align-items-center mb-5">
             Technical skills
           </Card.Title>
-          <Row className="justify-content-center align-items-center">
-            <Col md={4} sm={3} xs={4} className="mb-3">
-              <Card.Title className="sub-title">Type:</Card.Title>
-              {experience.technicalSkills.map((item) => (
-                <Card.Text className="card-text" key={item.id}>
-                  {item.skill_type}
-                </Card.Text>
-              ))}
-            </Col>
-            <Col md={4} sm={4} xs={5} className="mb-3">
-              <Card.Title className="sub-title">Name:</Card.Title>
-              {experience.technicalSkills.map((item) => (
-                <Card.Text className="card-text" key={item.id}>
-                  {item.skill_name}
-                </Card.Text>
-              ))}
-            </Col>
-            <Col md={3} sm={3} xs={3} className="mb-3">
-              <Card.Title className="sub-title">Level:</Card.Title>
-              {experience.technicalSkills.map((item) => (
-                <Card.Text className="card-text" key={item.id}>
-                  {item.level_of_experience}
-                </Card.Text>
-              ))}
-            </Col>
-          </Row>
+
+          {experience.technicalSkills.map((item) => (
+            <Row
+              key={item.id}
+              className="justify-content-center align-items-center mb-3"
+            >
+              <Col md={3} sm={4} xs={4} className="mb-3">
+                <Card.Text className="card-text">{item.skill_type}</Card.Text>
+              </Col>
+              <Col md={4} sm={5} xs={5} className="mb-3">
+                <Card.Text className="card-text">{item.skill_name}</Card.Text>
+              </Col>
+              <Col md={5} sm={6} xs={6} className="mb-3">
+                <ProgressBar
+                  now={levelProgressMap[item.level_of_experience] || 0}
+                  style={{ width: "100%" }}
+                  color={variables.backgroundColor}
+                />
+              </Col>
+            </Row>
+          ))}
         </Card.Body>
+
         <div className="title-underline"></div>
         {/* Education */}
         <Card.Body className="col-12 col-md-10">
-          <Card.Title className="titel-experience d-flex justify-content-center align-items-center">
+          <Card.Title className="titel-experience d-flex justify-content-center align-items-center mb-5">
             Education
           </Card.Title>
           <Row className="justify-content-center align-items-center ">
